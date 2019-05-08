fca102a3d63e0437766ef860fea5a6e0cb914cb9
beshoy <beshoyibraheem@gmail.com>
2019-05-08 13:01:29 +0200

    dashboard design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,5 +5,14 @@
 @endsection
 
 @section('content')
-    
+    <div class="row m-0">
+        <div class="col-md-6 offset-md-3 create-post m-t-20 m-b-10">
+            <h5 class="m-t-5">create post</h5>
+            <form method="" action="" class="row m-0">
+                @csrf
+                <textarea name="body" placeholder="What's in your mind ?" class="w-100"></textarea>
+                <button type="submit" class="btn m-t-5 m-b-5">create</button>
+            </form>
+        </div>
+    </div>
 @endsection
